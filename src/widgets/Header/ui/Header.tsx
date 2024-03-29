56a6ec793f367b5f9ec02118ea7bdcb265bd554d
IMaxOGI <max165773@gmail.com>
2024-03-29 15:50:37 +0200

    add chart js and some fix

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -50,104 +50,105 @@ export const Header: React.FC = () => {
         height={1300}
         className={s.Header__ellipse}
       />
-      <div className={s['Header__up-section']}>
-        <Link
-          href={routeLinks.home}
-          className={s['Header__up-section-logo']}
-        >
-          <Icon name={'logo'} /> Boldo
-        </Link>
-
-        <nav className={s['Header__up-section-navigation']}>
-          <ul className={s['Header__up-section-navigation-list']}>
-            <li className={s['Header__up-section-navigation-list-item']}>
-              <Link href={routeLinks.products}>Product</Link>
-            </li>
-            <li className={s['Header__up-section-navigation-list-item']}>
-              <Link href={routeLinks.services}>Services</Link>
-            </li>
-            <li className={s['Header__up-section-navigation-list-item']}>
-              <Link href={routeLinks.about}>About</Link>
-            </li>
-          </ul>
-          <Button
-            className={s['Header__up-section-navigation-list-item-button']}
-            variant={'filled-white'}
+      <div className={s['Header__wrapper']}>
+        <div className={s['Header__up-section']}>
+          <Link
+            href={routeLinks.home}
+            className={s['Header__up-section-logo']}
           >
-            Log in
-          </Button>
-        </nav>
+            <Icon name={'logo'} /> Boldo
+          </Link>
 
-        <div className={cn(s['Header__up-section-navigation-mobile'], isMenuOpen ? s['is-open'] : '')}>
-          {isMenuOpen && (
-            <div
-              onClick={toggleMenu}
-              className={s['Header__up-section-navigation-mobile-cross']}
-            >
-              <Icon name={'cross'} />
-            </div>
-          )}
-
-          <ul className={s['Header__up-section-navigation-mobile-list']}>
-            <li className={s['Header__up-section-navigation-mobile-list-item']}>
-              <Link href={routeLinks.products}>
-                Product <Icon name={'chevron'} />
-              </Link>
-            </li>
-            <li className={s['Header__up-section-navigation-mobile-list-item']}>
-              <Link href={routeLinks.services}>
-                Services <Icon name={'chevron'} />
-              </Link>
-            </li>
-            <li className={s['Header__up-section-navigation-mobile-list-item']}>
-              <Link href={routeLinks.about}>
-                About <Icon name={'chevron'} />
-              </Link>
-            </li>
-          </ul>
-          <Button
-            className={s['Header__up-section-navigation-mobile-list-item-button']}
-            variant={'filled-white'}
-          >
-            Log in
-          </Button>
-        </div>
-        <button
-          className={s['Header__up-section-menu-toggle']}
-          onClick={toggleMenu}
-        >
-          <Icon name={'menu'} />
-        </button>
-      </div>
-      <div className={s['Header__down-section']}>
-        <div>
-          <h1>Save time by building fast with Boldo Template </h1>
-          <p>
-            Funding handshake buyer business-to-business metrics iPad partnership. First mover advantage innovator
-            success deployment non-disclosure.
-          </p>
-          <div className={s['Header__down-section-buttons']}>
+          <nav className={s['Header__up-section-navigation']}>
+            <ul className={s['Header__up-section-navigation-list']}>
+              <li className={s['Header__up-section-navigation-list-item']}>
+                <Link href={routeLinks.products}>Product</Link>
+              </li>
+              <li className={s['Header__up-section-navigation-list-item']}>
+                <Link href={routeLinks.services}>Services</Link>
+              </li>
+              <li className={s['Header__up-section-navigation-list-item']}>
+                <Link href={routeLinks.about}>About</Link>
+              </li>
+            </ul>
             <Button
-              className={s['Header__down-section-buttons-button']}
-              variant={'filled-green'}
+              className={s['Header__up-section-navigation-list-item-button']}
+              variant={'filled-white'}
             >
-              Buy template
+              Log in
             </Button>
+          </nav>
+
+          <div className={cn(s['Header__up-section-navigation-mobile'], isMenuOpen ? s['is-open'] : '')}>
+            {isMenuOpen && (
+              <div
+                onClick={toggleMenu}
+                className={s['Header__up-section-navigation-mobile-cross']}
+              >
+                <Icon name={'cross'} />
+              </div>
+            )}
+
+            <ul className={s['Header__up-section-navigation-mobile-list']}>
+              <li className={s['Header__up-section-navigation-mobile-list-item']}>
+                <Link href={routeLinks.products}>
+                  Product <Icon name={'chevron'} />
+                </Link>
+              </li>
+              <li className={s['Header__up-section-navigation-mobile-list-item']}>
+                <Link href={routeLinks.services}>
+                  Services <Icon name={'chevron'} />
+                </Link>
+              </li>
+              <li className={s['Header__up-section-navigation-mobile-list-item']}>
+                <Link href={routeLinks.about}>
+                  About <Icon name={'chevron'} />
+                </Link>
+              </li>
+            </ul>
             <Button
-              className={s['Header__down-section-buttons-button']}
-              variant={'outlined-white'}
+              className={s['Header__up-section-navigation-mobile-list-item-button']}
+              variant={'filled-white'}
             >
-              Explore
+              Log in
             </Button>
           </div>
+          <button
+            className={s['Header__up-section-menu-toggle']}
+            onClick={toggleMenu}
+          >
+            <Icon name={'menu'} />
+          </button>
         </div>
-        <Image
-          src={images.heroGraphics}
-          alt="hero graphics"
-          className="img-fluid"
-        />
+        <div className={s['Header__down-section']}>
+          <div>
+            <h1>Save time by building fast with Boldo Template </h1>
+            <p>
+              Funding handshake buyer business-to-business metrics iPad partnership. First mover advantage innovator
+              success deployment non-disclosure.
+            </p>
+            <div className={s['Header__down-section-buttons']}>
+              <Button
+                className={s['Header__down-section-buttons-button']}
+                variant={'filled-green'}
+              >
+                Buy template
+              </Button>
+              <Button
+                className={s['Header__down-section-buttons-button']}
+                variant={'outlined-white'}
+              >
+                Explore
+              </Button>
+            </div>
+          </div>
+          <Image
+            src={images.heroGraphics}
+            alt="hero graphics"
+            className="img-fluid"
+          />
+        </div>
       </div>
-
       {isSwiperReady && (
         <Swiper
           className={s['Header__swiper']}
